de4fbc8025ab98e25cb30201853c1ff232b55412
HessahSaedan <hessahsaedan@gmail.com>
2022-04-19 10:55:07 +0300

    3 activites

--- app/src/main/java/com/example/saidan_midt2/MainActivity2.java
+++ app/src/main/java/com/example/saidan_midt2/MainActivity2.java
@@ -2,13 +2,77 @@ package com.example.saidan_midt2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity2 extends AppCompatActivity {
+    DatabaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText id = (EditText) findViewById(R.id.id);
+        EditText surname = (EditText) findViewById(R.id.surname);
+        TextView empty = (TextView) findViewById(R.id.empty);
+
+        Button insert = (Button) findViewById(R.id.insert);
+        Button nextt = (Button) findViewById(R.id.nextt);
+        Button back = (Button) findViewById(R.id.back);
+
+        myDB = new DatabaseHelper(this);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                name = Name.getText().toString();
+                surname = surname.getText().toString();
+                id = nationalid.getText().toString();
+
+
+                if (name.equals("") || surname.equals("") || id.equals("")) {
+                    Toast.makeText(MainActivity2.this,
+                            "Fields are empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!myDB.addData(name, surname, id))
+                    Toast.makeText(MainActivity2.this,
+                            "Insertion Failed", Toast.LENGTH_SHORT).show();
+                else {
+                    Toast.makeText(MainActivity2.this,
+                            "Insertion Success", Toast.LENGTH_SHORT).show();
+                }
+
+                Toasty.success(getBaseContext(), "Success.", Toast.LENGTH_SHORT, true).show();
+
+            }
+
+        });
+
+        nextt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/saidan_midt2/MainActivity3.java
+++ app/src/main/java/com/example/saidan_midt2/MainActivity3.java
@@ -3,6 +3,12 @@ package com.example.saidan_midt2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        EditText data = (EditText) findViewById(R.id.data);
+        Button input = (Button) findViewById(R.id.input);
+        Button all_data = (Button) findViewById(R.id.all_data);
+
+        input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (name.equals("") || surname.equals("") || id.equals("")) {
+                    Toast.makeText(MainActivity3.this,
+                            "Fields are empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(name.equals("a") || surname.equals("a") || id.equals("a")) {
+                    Toast.makeText(MainActivity3.this,
+                            "Fields are deleted", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toasty.error(getBaseContext(), "All record are deleted!", Toast.LENGTH_SHORT, true).show();
+            }
+        });
+
     }
 }
